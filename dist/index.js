34ab92a1c58663dc8507e66275e8497de0eaefd6
Priyansh Patidar <patidar.priyansh936@gmail.com>
2021-11-01 02:16:19 +0530

    removed dist folder from repo

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./client");
-const discord_js_1 = require("discord.js");
-new client_1.default({
-    intents: [discord_js_1.Intents.FLAGS.GUILDS],
-}).init();
-//# sourceMappingURL=index.js.map
